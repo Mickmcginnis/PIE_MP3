adf7d421fea8e93b3c53dd9909fc178f9bc4d359
reisenbud <ruby.eisenbud@gmail.com>
2021-10-14 11:26:14 -0400

    Testing corners

--- MP3_1/MP3_1.ino
+++ MP3_1/MP3_1.ino
@@ -10,22 +10,24 @@ int sensor1 = 0; // left IR sensor
 int sensor2 = 1; // right IR sensor
 float Sright;    // right sensor value
 float Sleft;     // left sensor value
-int threshold;   // IR value threshold used to determine if sensor is on ground or on tape
+int threshold = 300;   // IR value threshold used to determine if sensor is on ground or on tape
+//int delaytime = 20;
 
 // define motor speeds for various cases
-float Mleft_move_speed = 30;
-float Mright_move_speed = 30;
-float Mleft_still_speed = 0;
-float Mright_still_speed = 0;
+float Mleft_move_speed = 60;
+float Mright_move_speed = 60;
+float Mleft_still_speed = 30;
+float Mright_still_speed = 30;
+uint8_t rundirection = BACKWARD;
 
 unsigned int run_time;
-const int check_time = 50;
+const int check_time = 20;
 
 void setup() {
   // put your setup code here, to run once:
 
   AFMS.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(sensor1, OUTPUT);
   pinMode(sensor2, OUTPUT);
   run_time = millis();
@@ -35,7 +37,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   unsigned int t;
   t = millis();
-  threshold = 300;
+  //threshold = 200;
 
   if (t >= run_time + check_time) {
     Sright = analogRead(sensor2);
@@ -48,7 +50,7 @@ void loop() {
       Mleft->setSpeed(Mleft_move_speed);
       Mleft->run(FORWARD);
       Mright->setSpeed(Mright_still_speed);
-      Mright->run(FORWARD);
+      Mright->run(rundirection);
 
       // Print in format: (Sleft, Mleft, Sright, Mright)
       Serial.print("(");
@@ -60,7 +62,7 @@ void loop() {
       Serial.print(", ");
       Serial.print(Mright_still_speed);
       Serial.print(")\n");
-      delay(50);
+      //delay(20);
     }
 
     else if (Sright < threshold && Sleft > threshold) { //Sleft on line
@@ -68,7 +70,7 @@ void loop() {
       Mright->setSpeed(Mright_move_speed);
       Mright->run(FORWARD);
       Mleft->setSpeed(Mleft_still_speed);
-      Mleft->run(FORWARD);
+      Mleft->run(rundirection);
 
       // Print in format: (Sleft, Mleft, Sright, Mright)
       Serial.print("(");
@@ -80,7 +82,7 @@ void loop() {
       Serial.print(", ");
       Serial.print(Mright_move_speed);
       Serial.print(")\n");
-      delay(50);
+      //delay(20);
     }
     else if (Sright < threshold && Sleft < threshold) {
       // Move both motors forward
@@ -99,7 +101,24 @@ void loop() {
       Serial.print(", ");
       Serial.print(Mright_move_speed);
       Serial.print(")\n");
-      delay(50);
+      //delay(50);
+    }
+    else if(Sright > threshold && Sleft > threshold){
+      Mleft->setSpeed(Mleft_move_speed);
+      Mleft->run(FORWARD);
+      Mright->setSpeed(Mright_move_speed);
+      Mright->run(FORWARD);
+
+      // Print in format: (Sleft, Mleft, Sright, Mright)
+      Serial.print("(");
+      Serial.print(Sleft);
+      Serial.print(", ");
+      Serial.print(Mleft_move_speed);
+      Serial.print(", ");
+      Serial.println(Sright);
+      Serial.print(", ");
+      Serial.print(Mright_move_speed);
+      Serial.print(")\n");
     }
 
     //From Adafruit documentation, 0=stop, 255=full speed
